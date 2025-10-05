0ad9917a855f978eaefb6e813db57217ba17084a
Wesley Oates <wesley.oates@gmail.com>
2025-10-05 15:01:10 -0600

    New unit testing

--- tests/d20/test_orientation.py
+++ tests/d20/test_orientation.py
@@ -3,7 +3,7 @@ from delta20.packing import pack_vertex_idx, unpack_face_idx
 from testutils import get_canonicals, is_ccw_on_sphere
 
 
-def test_faces_are_ccw_and_apex_polarity_matches_orientation():
+def test_faces_are_ccw_and_apex_polarity_matches_orientation_y_up():
     verts, faces, _ = get_canonicals()
     north_count = 0
     south_count = 0
@@ -15,13 +15,15 @@ def test_faces_are_ccw_and_apex_polarity_matches_orientation():
         # polarity bit from packed face id
         lod, d20, path, south = unpack_face_idx(
             fid)  # adjust to your signature
-        apex_z = p0[2]
+
+        # with y-up, the apex is the pole-most vertex by y
+        apex_y = p0[1]
         if south:
             south_count += 1
-            assert apex_z < 0.0
+            assert apex_y < 0.0
         else:
             north_count += 1
-            assert apex_z > 0.0
+            assert apex_y > 0.0
 
     # distribution sanity (5 north-cap, 5 south-cap, 10 alternating)
     assert north_count == 10
